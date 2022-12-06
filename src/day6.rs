4506734353554a5830b9419bbc8ba30d7dd57bca
Henrik Lundberg <velfolt@gmail.com>
2022-12-06 07:28:44 +0100

    refactor dec 6

--- src/day6.rs
+++ src/day6.rs
@@ -2,20 +2,27 @@ use std::collections::HashSet;
 
 use crate::file::read_lines;
 
+fn find_marker(n: usize, bytes: &[u8]) -> Option<usize> {
+    for (index, window) in bytes.windows(n).enumerate() {
+        let mut hash = HashSet::new();
+        for element in window {
+            hash.insert(*element);
+        }
+
+        if hash.len() == n {
+            return Some(index + n);
+        }
+    }
+
+    None
+}
+
 pub fn part_1() {
     let lines = read_lines("day6.txt").map(|x| x.unwrap());
 
     for line in lines {
-        for (index, window) in line.as_bytes().windows(4).enumerate() {
-            let mut hash = HashSet::new();
-            for element in window {
-                hash.insert(*element);
-            }
-
-            if hash.len() == 4 {
-                println!("{:?}", index + 4);
-                break;
-            }
+        if let Some(marker) = find_marker(4, &line.as_bytes()) {
+            println!("{:?}", marker);
         }
     }
 }
@@ -29,16 +36,8 @@ pub fn part_2() {
     let lines = read_lines("day6.txt").map(|x| x.unwrap());
 
     for line in lines {
-        for (index, window) in line.as_bytes().windows(14).enumerate() {
-            let mut hash = HashSet::new();
-            for element in window {
-                hash.insert(*element);
-            }
-
-            if hash.len() == 14 {
-                println!("{:?}", index + 14);
-                break;
-            }
+        if let Some(marker) = find_marker(14, &line.as_bytes()) {
+            println!("{:?}", marker);
         }
     }
 }
